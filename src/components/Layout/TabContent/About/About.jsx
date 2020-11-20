1bd68d2b1d442a9afa155e99aae8117592bec763
Paul Rupke <paul.rupke@gmail.com>
2020-11-19 19:48:37 -0500

    Add basic about page

--- src/components/Layout/TabContent/About/About.jsx
+++ src/components/Layout/TabContent/About/About.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 export default function About(props) {
   return (
     <div>
-      About
+      <h2>Hélène Macé</h2>
+      <p>Food photographer</p> 
     </div>
   )
 }
