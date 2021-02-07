3e5826912fa797ff3aa743349183b7f71d7380ad
Paul Rupke <paul.rupke@gmail.com>
2021-02-06 22:57:17 -0500

    fade in content as it scrolls into view

--- src/components/Layout/TabContent/Portfolio/Portfolio.jsx
+++ src/components/Layout/TabContent/Portfolio/Portfolio.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react'; 
 import Photo from './Photo/Photo';
 import './Portfolio.css';
 
 export default function Photos(props) {
+
+  function FadeInSection(props) {
+    const [isVisible, setVisible] = useState(true);
+    const domRef = useRef();
+    useEffect(() => {
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => setVisible(entry.isIntersecting));
+      });
+      observer.observe(domRef.current);
+      return () => observer.unobserve(domRef.current);
+    }, []);
+    return (
+      <div
+        className={`fade-in-section ${isVisible && 'is-visible'}`}
+        ref={domRef}
+      >
+        {props.children}
+      </div>
+    );
+  }
+
   return (
     <section>
       <div className="buffer" />
       <ul>
         {props.photos && (
           props.photos.map ((photo, index) =>
-            <li>
-              <Photo 
-                key={index}
-                url={photo.url}
-              />
-            </li>
+            <FadeInSection key={index}>
+              <li className="photo">
+                <Photo 
+                  key={index}
+                  url={photo.url}
+                />
+              </li>
+            </FadeInSection>
           )
         )}
       </ul>
