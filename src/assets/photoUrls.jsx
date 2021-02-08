e6575f46c7d868c4ce50603d55f88a9a8f00e13c
Paul Rupke <paul.rupke@gmail.com>
2021-02-07 20:22:38 -0500

    import new photos

--- src/assets/photoUrls.jsx
+++ src/assets/photoUrls.jsx
@@ -1,5 +1,37 @@
-// example import:
-// import chocolate from '../assets/chocolate.jpg';
+import basilLeaf from '../assets/Basilleaf.jpg';
+import caramel1 from '../assets/Caramel1.jpg';
+import caramel2 from '../assets/Caramel2.jpg';
+import caramel3 from '../assets/Caramel3.jpg';
+import caramel4 from '../assets/Caramel4.jpg';
+import caramel5 from '../assets/Caramel5.jpg';
+import caramel6 from '../assets/Caramel6.jpg';
+import caramel7 from '../assets/Caramel7.jpg';
+import caramel8 from '../assets/Caramel8.jpg';
+import caramel9 from '../assets/Caramel9.jpg';
+import chocolate from '../assets/Chocolate.jpg';
+import chocolateBrownieCookie1 from '../assets/Chocolatebrowniescookies1.jpg';
+import chocolateBrownieCookie2 from '../assets/Chocolatebrowniescookies2.jpg';
+import chocolateBrownieCookie3 from '../assets/Chocolatebrowniescookies3.jpg';
+import chocolateTart from '../assets/Chocolatetart.jpg';
+import choux from '../assets/Choux.jpg';
+import gianduja from '../assets/Gianduja.jpg';
+import madeleine1 from '../assets/Madeleine1.jpg';
+import madeleine2 from '../assets/Madeleine2.jpg';
+import parisBrest3 from '../assets/Paris-Brest3.jpg';
+import praline1 from '../assets/Praline1.jpg';
+import purplePeppers1 from '../assets/Purplepeppers1.jpg';
+import purplePeppers2 from '../assets/Purplepeppers2.jpg';
+import raspberryTart from '../assets/Raspberrytart.jpg';
+import tarteAuxFraises from '../assets/Tarteauxfraises.jpg';
+import waffleBite from '../assets/Wafflebite.jpg';
+import waffleBreakfastTray from '../assets/Wafflebreakfasttray.jpg';
+import waffleDrizzle from '../assets/Waffledrizzle.jpg';
+import wafflePlate from '../assets/Waffleplate.jpg';
+import waffles1 from '../assets/Waffles1.jpg';
+import waffles2 from '../assets/Waffles2.jpg';
+import waffles3 from '../assets/Waffles3.jpg';
+import waffleSandwich from '../assets/Wafflesandwich.jpg';
+import wafflesStack from '../assets/Wafflestack.jpg';
 
 //  to test with placeholders and set the size:
 //     {
@@ -8,43 +40,106 @@
 
 export const photoURLs = [
   {
-    url: "https://via.placeholder.com/300x500",
+    url: basilLeaf,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel1,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel2,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel3,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel4,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel5,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel6,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel7,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel8,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: caramel9,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: chocolate,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: chocolateBrownieCookie1,
   },
   {
-    url: "https://via.placeholder.com/300x500",
+    url: chocolateBrownieCookie2,
+  },
+  {
+    url: chocolateBrownieCookie3,
+  },
+  {
+    url: chocolateTart,
+  },
+  {
+    url: choux,
+  },
+  {
+    url: gianduja,
+  },
+  {
+    url: madeleine1,
+  },
+  {
+    url: madeleine2,
+  },
+  {
+    url: parisBrest3,
+  },
+  {
+    url: praline1,
+  },
+  {
+    url: purplePeppers1,
+  },
+  {
+    url: purplePeppers2,
+  },
+  {
+    url: raspberryTart,
+  },
+  {
+    url: tarteAuxFraises,
+  },
+  {
+    url: waffleBite,
+  },
+  {
+    url: waffleBreakfastTray,
+  },
+  {
+    url: waffleDrizzle,
+  },
+  {
+    url: wafflePlate,
+  },
+  {
+    url: waffles1,
+  },
+  {
+    url: waffles2,
+  },
+  {
+    url: waffles3,
+  },
+  {
+    url: waffleSandwich,
+  },
+  {
+    url: wafflesStack,
   },
 ]
 
