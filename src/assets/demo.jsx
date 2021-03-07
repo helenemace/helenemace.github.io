ec7dbaa47463cbb529c004318b245e9f6c826360
Paul Rupke <paul.rupke@gmail.com>
2021-03-07 10:26:55 -0500

    Add demo test

---
+++ src/assets/demo.jsx
@@ -0,0 +1,82 @@
+import basilLeaf from '../assets/Basilleaf.jpg';
+import caramel1 from '../assets/Caramel1.jpg';
+import caramel2 from '../assets/Caramel2.jpg';
+import caramel3 from '../assets/Caramel3.jpg';
+import caramel4 from '../assets/Caramel4.jpg';
+import caramel5 from '../assets/Caramel5.jpg';
+import caramel6 from '../assets/Caramel6.jpg';
+import caramel7 from '../assets/Caramel7.jpg';
+import caramel8 from '../assets/Caramel8.jpg';
+import caramel9 from '../assets/Caramel9.jpg';
+import chocolate from '../assets/Chocolate.jpg';
+import chocolateBrownieCookie1 from '../assets/Chocolatebrowniescookies1.jpg';
+import chocolateBrownieCookie2 from '../assets/Chocolatebrowniescookies2.jpg';
+import chocolateBrownieCookie3 from '../assets/Chocolatebrowniescookies3.jpg';
+import chocolateTart from '../assets/Chocolatetart.jpg';
+import choux from '../assets/Choux.jpg';
+import gianduja from '../assets/Gianduja.jpg';
+import madeleine1 from '../assets/Madeleine1.jpg';
+import madeleine2 from '../assets/Madeleine2.jpg';
+import parisBrest3 from '../assets/Paris-Brest3.jpg';
+import praline1 from '../assets/Praline1.jpg';
+import purplePeppers1 from '../assets/Purplepeppers1.jpg';
+import purplePeppers2 from '../assets/Purplepeppers2.jpg';
+import raspberryTart from '../assets/Raspberrytart.jpg';
+import tarteAuxFraises from '../assets/Tarteauxfraises.jpg';
+import waffleBite from '../assets/Wafflebite.jpg';
+import waffleBreakfastTray from '../assets/Wafflebreakfasttray.jpg';
+import waffleDrizzle from '../assets/Waffledrizzle.jpg';
+import wafflePlate from '../assets/Waffleplate.jpg';
+import waffles1 from '../assets/Waffles1.jpg';
+import waffles2 from '../assets/Waffles2.jpg';
+import waffles3 from '../assets/Waffles3.jpg';
+import waffleSandwich from '../assets/Wafflesandwich.jpg';
+import wafflesStack from '../assets/Wafflestack.jpg';
+
+export const photos = [
+  {
+    src: basilLeaf,
+    width: 4,
+    height: 3
+  },
+  {
+    src: caramel1,
+    width: 4,
+    height: 3
+  },
+  {
+    src: caramel2,
+    width: 3,
+    height: 2
+  },
+  {
+    src: caramel3,
+    width: 3,
+    height: 3
+  },
+  {
+    src: caramel4,
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    width: 3,
+    height: 4
+  },
+  {
+    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    width: 4,
+    height: 3
+  }
+];
