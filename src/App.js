ebcad22b3afb99e2a3f4818fb56eb6c9301ff672
Paul Rupke <paul.rupke@gmail.com>
2022-07-25 17:09:58 -0400

    remove commented lines from app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-// import logo from './logo.svg';
 import Layout from './components/Layout/Layout';
-// import Gallery from './components/Gallery/Gallery';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Gallery /> */}
       <Layout />
     </div>
   );
